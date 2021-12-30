4cf4ebca16a5fec9545c0c7126df7393e5859f21
MHargrove96 <86787734+MHargrove96@users.noreply.github.com>
2021-12-29 18:58:41 -0600

    Just saving my work but no real functionality added

--- main.js
+++ main.js
@@ -2,19 +2,20 @@
 
 const assert = require('assert');
 const readline = require('readline');
+const standalone = require('stylelint/lib/standalone');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 // An object that represents the three stacks of Towers of Hanoi; 
-  // * each key is an array of Numbers: 
-    // * A is the far-left, 
-    // * B is the middle, 
-    // * C is the far-right stack
-      // * Each number represents the largest to smallest tokens: 
-        // * 4 is the largest, 
-        // * 1 is the smallest
+// * each key is an array of Numbers: 
+// * A is the far-left, 
+// * B is the middle, 
+// * C is the far-right stack
+// * Each number represents the largest to smallest tokens: 
+// * 4 is the largest, 
+// * 1 is the smallest
 
 let stacks = {
   a: [4, 3, 2, 1],
@@ -29,22 +30,37 @@ const printStacks = () => {
   console.log("c: " + stacks.c);
 }
 
-// Next, what do you think this function should do?
 const movePiece = (startingStack, endingStack) => {
-  // move the last item in an array from the startStack the the last item in the endStack 
-  // console.log('here', stacks[startingStack])
-  if(isLegal()){
-    stacks[endingStack].push(stacks[startingStack].pop())
-    checkForWin()
-  }
+  stacks[endingStack].push(stacks[startingStack].pop())
+  console.log('movePiece() being called')
+  // because isLegal() is never returning either of my first if statment move peice is not being called, but does work standalone ********** 
 }
 
 // Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = () => {
-  // check if the data-Size of the stone clicked is larger then the last stone in the endStack array 
-  if(stacks[endingStack] < stacks[startingStack])
+const isLegal = (startingStack, endingStack) => {
+  let firstPiece = stacks[startingStack].toString()
+  let secondPiece = stacks[endingStack].toString()
+  let lastOfStarting = firstPiece.charAt(firstPiece.length - 1)
+  let lastOfEnding = secondPiece.charAt(secondPiece.length - 1)
+  // this feel like the longest way possible to get the affect i was after ********** 
 
+  console.log(lastOfStarting, 'lastOfStarting')
+  console.log(lastOfEnding, 'lastOfEnding')
+  // i do not know what lastOfEnding is before the first number is in that column null or undefined ********** 
+  console.log('isLegal() is being called')
+
+  if (lastOfStarting < lastOfEnding) {
+    movePiece(startingStack, endingStack);
+    console.log('first if')
+  } else if (lastOfStarting == 1 && lastOfEnding == null) {
+    console.log('second if')
+    movePiece(startingStack, endingStack);
+  } else {
+    console.log('invalid move')
+  }
+  // something is wrong with my first to if statments cause they are never being called ********** 
 }
+
 
 // What is a win in Towers of Hanoi? When should this function run?
 const checkForWin = () => {
@@ -52,17 +68,24 @@ const checkForWin = () => {
   // a: [],
   // b: [],
   // c: [4, 3, 2, 1]
+  if (stacks.a == [] && stacks.b == [] && stacks.c == [4, 3, 2, 1]) {
+    return true
+  } else {
+    return false
+  }
+  // is there a better way to do this? / not even sure this works cant test **********
 }
 
 // When is this function called? What should it do with its argument?
 const towersOfHanoi = (startStack, endStack) => {
-  // Your code here
   console.log('start', startStack)
   console.log('end', endStack)
-  movePiece(startStack, endStack)
-
+  // movePiece(startStack, endStack)
+  // movePiece() works on it own the problem is still isLegal() **********
+  isLegal(startStack, endStack)
+  // trying to pass through isLegal() first ********** 
 }
-
+// ---------------------------------- test --------------------------------- test --------------------------- test -----------------------------------------------------------------
 const getPrompt = () => {
   printStacks();
   rl.question('start stack: ', (startStack) => {
@@ -104,9 +127,9 @@ if (typeof describe === 'function') {
   });
   describe('#checkForWin()', () => {
     it('should detect a win', () => {
-      stacks = { a: [], b: [4, 3, 2, 1], c: [] };
+      stacks = { a: [], b: [4, 3, 2, 1], c: [] }; // do i need to change this to stack c or is it testing this way for a reason?
       assert.equal(checkForWin(), true);
-      stacks = { a: [1], b: [4, 3, 2], c: [] };
+      stacks = { a: [1], b: [4, 3, 2], c: [] };  // kinda the same question as above for this test. 
       assert.equal(checkForWin(), false);
     });
   });
